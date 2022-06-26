360ea50c2e203b75bbfa460ddfbab310b5d31e76
Danilo Batista <developer@danilobatista.com>
2022-06-26 18:23:49 -0300

    dhFS M01-U03 | Exercise 2 - Operations with number.

---
+++ unid03_logica_com_js/exercise02.js
@@ -0,0 +1,23 @@
+/**
+ * Crie 2 variáveis com valores numéricos e depois, realize as quatro operações
+aritméticas com essas variáveis (+ , - , * , /) . Armazene o resultado de cada operação
+aritmética em uma nova variável. Exemplo:
+var soma = variavel1 + variavel2
+ */
+
+"use strict";
+
+let firstNumber = 24;
+let secondNumber = 8;
+
+const sum = firstNumber + secondNumber;
+const subtraction = firstNumber - secondNumber;
+const multiplication = firstNumber * secondNumber;
+const division = firstNumber / secondNumber;
+
+console.log("First number: ", firstNumber);
+console.log("Second number: ", secondNumber);
+console.log(sum);
+console.log(subtraction);
+console.log(multiplication);
+console.log(division);
