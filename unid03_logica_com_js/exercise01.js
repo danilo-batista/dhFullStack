206e56e0db965186807e4a0dfc7048192a78af0e
Danilo Batista <developer@danilobatista.com>
2022-06-26 18:18:17 -0300

    dhFS M01-U03 | Exercise 1 - Press card.

---
+++ unid03_logica_com_js/exercise01.js
@@ -0,0 +1,15 @@
+/**
+ * Crie 3 variáveis, uma contendo seu nome, outra contendo seu sobrenome e a última
+contendo sua profissão. Junte as 3 informações um uma quarta variável chamada
+cartão de visita e depois imprima o resultado usando o console.log
+OBS: Não se esqueça dos espaços entre as palavras na hora da exibição!
+ */
+
+"use strict";
+
+let firstName = "Danilo";
+let lastName = "Batista"
+let jobTitle = "Graphic Designer and Developer"
+let pressCard = firstName + " " + lastName + "\n" + jobTitle;
+
+console.log(pressCard);
