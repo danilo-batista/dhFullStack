dc40c2c7cda35681a552ebb6474a13dbb062bdbe
Danilo Batista <developer@danilobatista.com>
2022-06-26 21:33:18 -0300

    dhFS M01-U03 | Exercise 4 - Relational operators.

---
+++ unid03_logica_com_js/exercise04.js
@@ -0,0 +1,24 @@
+/**
+ * Crie 2 variáveis numéricas, atribua valores a elas e em seguida, compare os valores
+utilizando os operadores relacionais, exibindo o resultado através do console.log
+ */
+
+"use strict";
+
+let firstNumber = 20;
+let secondNumber = 10;
+console.log("\nPrimeiro número: " + firstNumber);
+console.log("Segundo número: " + secondNumber);
+console.log("- Valor de A > B?");
+console.log(firstNumber > secondNumber);
+console.log("- Valor de A < B?");
+console.log(firstNumber < secondNumber);
+console.log("- Valor de A >= B?");
+console.log(firstNumber >= secondNumber);
+console.log("- Valor de A <= B?");
+console.log(firstNumber <= secondNumber);
+console.log("- Valor de A == B?");
+console.log(firstNumber == secondNumber);
+console.log("- Valor de A === B?");
+console.log(firstNumber === secondNumber);
+
