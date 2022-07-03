1ebeeb75288cedbb04ffb1e672c9a957b47b731f
Danilo Batista <developer@danilobatista.com>
2022-07-03 12:27:40 -0300

    dhFS M01-U05 | Exercise 3 - Print numbers.

---
+++ unid05_estruturas_de_repeticao/exercise3_printNumbers.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/**
+ * Crie um loop que execute 15 repetições e cada ciclo desse laço deve imprimir o
+valor da variável contadora, que nesse caso irá começar no 1.
+Por exemplo, o resultado no terminal precisa ser esse:
+1
+2
+3
+4… e assim por diante.
+ */
+for (counter = 1; counter <= 15; counter++) {
+    console.log(counter);
+}
