45b737bc3d4f4baad38d5199972f65b028da1385
Danilo Batista <developer@danilobatista.com>
2022-07-03 12:45:13 -0300

    dhFS M01-U05 | Exercise 6 - Print numbers with While.

---
+++ unid05_estruturas_de_repeticao/exercise6_printNumberWithWhile.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/**
+ * Crie um laço de repetição While que irá iterar enquanto o valor de i for menor ou
+igual a 15 e mostre no console o valor de cada iteração.
+ */
+let counter = 1;
+
+while (counter <= 15) {
+    console.log(counter);
+    counter++;
+}
