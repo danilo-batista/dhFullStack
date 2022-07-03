d81fb2531a0870bd033f309ff095b89202d102d5
Danilo Batista <developer@danilobatista.com>
2022-07-03 12:15:21 -0300

    dhFS M01-U04 | Examples.

---
+++ unid05_estruturas_de_repeticao/examples.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/** Exemplo 1
+ * Uma Professora de Matemática está com dificuldades para ensinar os múltiplos de 10
+para sua turma, então ela decidiu utilizar a programação para atrair a atenção dos seus
+alunos. Porém, ela não sabe programar e pediu nossa ajuda para criar uma aplicação
+que imprima os valores de 1 a 100, e para cada vez que a impressão encontrar um valor
+múltiplo de 10, imprima também, a frase: “ - Múltiplo de 10”.
+
+for (let contador = 1; contador <= 100; contador++) {
+    if (contador % 10 == 0) {
+        console.log(contador, " - Múltiplo de 10");
+    } else {
+        console.log(contador);
+    }
+}
+
+ */
+
+/** Exemplo 2
+ * Vamos criar um loop for, que irá imprimir no console a seguinte frase “Estou
+aprendendo Loops” 10 vezes.
+
+for (let contador = 1; contador <= 100; contador++) {
+    console.log("Estou aprendendo loops!");
+}
+
+ */
