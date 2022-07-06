adf47ebfe85dabac953f2f4a1fc81e8f5dda85c2
Danilo Batista <developer@danilobatista.com>
2022-07-06 01:56:28 -0300

    dhFS M01-U04 | Challenge - Country selector with Switch case.

---
+++ unid04_estruturas_de_decisao/challenge_countrySelector.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/** Desafio de aula - Condicionais
+ * 
+ * 
+ * 
+1 - Africa
+2 - America
+3 - Asia
+4 - Europa
+5 - Oceania
+
+Digite o número do continente e veja quais países existem nele!
+ */
+
+let continent;
+
+console.log("\n************* Planejamento de Viagem *************\n");
+console.log("\tEscolha o continente para conhecer os países disponíveis para viagem.");
