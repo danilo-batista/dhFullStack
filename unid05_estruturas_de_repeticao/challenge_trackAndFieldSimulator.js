1f51ac2ca4a1a11a48a0c79a440f6e47540d960b
Danilo Batista <developer@danilobatista.com>
2022-07-10 22:09:04 -0300

    dhFS M01-U05 | Challenge - Land travel simulator with 'while'.

--- unid05_estruturas_de_repeticao/challenge_trackAndFieldSimulator.js
+++ unid05_estruturas_de_repeticao/challenge_trackAndFieldSimulator.js
@@ -18,9 +18,9 @@ console.log("\n\tAo final da sua simulação, você saberá:\n\t\t1. Quantidade 
 let i = 1;
 checkpointsList = parseInt(prompt("\tInsira a " + i + "ª parada(checkpoint): "));
 while (checkpointsList != 0) {
-    console.log("\t∙ Parada " + i + ": " + checkpointsList.toLocaleString("pt-Br") + "km | (0) Finalizar Simulação");
+    console.log("\t∙ Parada " + (i) + ": " + checkpointsList.toLocaleString("pt-Br") + "km | (0) Finalizar Simulação");
     travelDistance = travelDistance + checkpointsList;
-    checkpointsList = parseInt(prompt("\tInsira a " + i + "ª parada(checkpoint): "));
+    checkpointsList = parseInt(prompt("\tInsira a " + (i + 1) + "ª parada(checkpoint): "));
     i++;
 }
 console.log("\n\tSeu percurso terá " + travelDistance.toLocaleString("pt-Br") + "km com " + (i - 1) + " paradas(checkpoints).\n");
