e244667b28b84b6cc28c342df483dc030fbf98a3
Danilo Batista <developer@danilobatista.com>
2022-07-16 16:32:21 -0300

    dhFS M01-U06 | Fintech: refactoring to correct some bugs and improve UI.

--- m01_u06_fintech-challenge/challenge_fintech.js
+++ m01_u06_fintech-challenge/challenge_fintech.js
@@ -1,128 +1,176 @@
 const prompt = require("prompt-sync")()
-// ConsultaSaldo
-// Transferência
-// Saque
-// Deposito
-// Histórico 
-var saldo1 = 1500.00;
-var saldo2 = 1200.00;
-var historico = []
 
-function consultaSaldo(saldo) {
+/** Challenge | Aula 6
+  * > Digital House FS
+  * > Autores: Nome1, Nome2, Nome3, Nome4, Nome5, Nome6
+  **/
 
-    historico.push("\nconsulta  " + Date().toLocaleString("pt-BR"))
-    return saldo;
+let saldo1 = 1500.00;
+let saldo2 = 1200.00;
+const historico = [];
+
+/* Consultar saldo */
+function consultarSaldo(saldo) {
+    historico.push("  Consulta | " + Date().toLocaleString("pt-BR"));
+    return saldo.toLocaleString("pt-Br");
 }
 
-function saque(saldoConta, saque) {
-    historico.push("\nsaque  " + Date().toLocaleString("pt-BR"))
-    if (saque <= 0) {
-        console.log("Saque não permitido")
+/* Sacar dinheiro */
+function sacarDinheiro(saldoConta, valorASerSacado) {
+    historico.push("  Saque | " + Date().toLocaleString("pt-BR"));
+    if (valorASerSacado <= 0) {
+        console.log("\n\tSaque não permitido!\n");
     } else {
-        saldoConta = saldoConta - saque
-    }
-    return saldoConta
-}
-// regra saca do primerio parametro e deposita no segundo parametro o valor do 3º teceito
-function transferencia(saldo, saldo2, valor) {
-    saldo1 = saque(saldo, valor)
-    saldo2 = deposito(saldo2, valor)
-    historico.pop()
-    historico.pop()
-    historico.push("\ntrasferencia  " + Date().toLocaleString("pt-BR"))
-}
-
-function deposito(saldoConta, valor) {
-    historico.push("\ndeposito  " + Date().toLocaleString("pt-BR"))
-
-    if (valor > 0) {
-        saldoConta = saldoConta + valor
-    }
-    else {
-        console.log("Deposito não permitido valor inválido! ")
+        saldoConta = saldoConta - valorASerSacado;
     }
     return saldoConta;
 }
 
-function historicoAcoes() {
-    for (let index = 0; index < historico.length; index++) {
-        console.log("__________")
-        console.log('       ', historico[index].toUpperCase());
+/* Depositar dinheiro */
+function depositarDinheiro(saldoConta, valorASerDepositado) {
+    historico.push("  Depósito | " + Date().toLocaleString("pt-BR"));
+
+    if (valorASerDepositado > 0) {
+        saldoConta = saldoConta + valorASerDepositado;
+    } else {
+        console.log("\n\tDepósito não permitido! Valor inválido!\n");
     }
+    return saldoConta;
 }
 
+/* Transferir dinheiro (sacar da conta A e depositar na conta B) */
+function transferirDinheiro(saldoAtual, saldoDestino, valor) {
+    saldo1 = sacarDinheiro(saldoAtual, valor);
+    saldo2 = depositarDinheiro(saldoDestino, valor);
+    historico.pop(historico.pop());
+    historico.push("  Transferência | " + Date().toLocaleString("pt-BR"));
+}
+
+/* Listagem do histórico de ações realizadas */
+function listarAcoes() {
+    console.log("\n\t-------------------------------------------------------------------------\n");
+    for (let index = 0; index < historico.length; index++) {
+        console.log("\t" + historico[index].toUpperCase());
+    }
+    console.log("\n\t-------------------------------------------------------------------------\n");
+}
+
+/* Execução */
+console.log("\n------------------------ Bem-Vindo ao Digital Bank ----------------------------\n");
+console.log("\n\tEscolha a sua opção para iniciar: [1] Menu | [0] Sair\n");
+
+let continuarExecucao = parseInt(prompt("\t\tOpção: "));
+
+// loop que deixa o aplicativo voltando para o menu inicial
 while (true) {
-    var seguir = parseInt(prompt("Deseja seguir no programa ? Digite 1 para seguir ou 0 para encerrar:  "))
-    if (seguir == 1) {
-        var opcao = parseInt(prompt("Digite uma das opções:\n1 para ver o saldo \n2 para realizar o saque \n3 para realizar um deposito \n4 para trasferencia \n5 para ver o histórico de ações realizadas "))
-    } else if (seguir == 0) {
+    let menuOpcoes;
+
+    // menu de opções
+    if (continuarExecucao == 1) {
+        console.log("\n\tOperações bancárias:\n\n\t[1] Saldo\n\t[2] Saque\n\t[3] Depósito\n\t[4] Transferência\n\t[5] Histórico\n");
+        menuOpcoes = parseInt(prompt("\tOperação escolhida: "));
+
+        switch (menuOpcoes) {
+            case 1: //consultarSaldo
+                console.log("\n\t-------------------------------------------------------------------------\n");
+                console.log("\t• Selecione uma conta cadastrada para prosseguir: [1] Conta_1 | [2] Conta_2\n");
+
+                var conta = parseInt(prompt("\tConta escolhida: "));
+
+                if (conta == 1) {
+                    console.log("\n\t>>> Conta_1 | SALDO: R$ " + consultarSaldo(saldo1) + "\n");
+                    console.log("-------------------------------------------------------------------------------------\n")
+                } else if (conta == 2) {
+                    console.log("\n\t>>> Conta_2 | SALDO: R$ " + consultarSaldo(saldo2) + "\n");
+                    console.log("-------------------------------------------------------------------------------------\n")
+                } else {
+                    console.log("\n\t*** Visualização do saldo cancelada. Conta inexistente! Escolha uma conta válida! ***\n");
+                    console.log("-------------------------------------------------------------------------------------\n")
+                }
+                break;
+            case 2: // sacar
+                console.log("\n\t-------------------------------------------------------------------------\n");
+
+                console.log("\t• Selecione uma conta cadastrada para prosseguir: [1] Conta_1 | [2] Conta_2\n");
+                conta = parseInt(prompt("\tConta escolhida: "));
+                console.log("");
+
+                let valorASerSacado = parseInt(prompt("\t• Insira o valor que deseja sacar: R$ "));
+
+                if (conta == 1) {
+                    saldo1 = sacarDinheiro(saldo1, valorASerSacado);
+                    console.log("\n\t>>> Conta_1 | SALDO: R$ " + consultarSaldo(saldo1) + "\n");
+                    console.log("-------------------------------------------------------------------------------------\n")
+                } else if (conta == 2) {
+                    saldo2 = sacarDinheiro(saldo2, valorASerSacado);
+                    console.log("\n\t>>> Conta_2 | SALDO: R$ " + consultarSaldo(saldo2) + "\n");
+                    console.log("-------------------------------------------------------------------------------------\n")
+                } else {
+                    console.log("\n\t*** Saque cancelado. Conta inexistente! Escolha uma conta válida! ***\n");
+                    console.log("-------------------------------------------------------------------------------------\n")
+                }
+
+                break;
+            case 3: // depositar
+                console.log("\n\t-------------------------------------------------------------------------\n");
+
+                console.log("\t• Selecione uma conta cadastrada para prosseguir: [1] Conta_1 | [2] Conta_2\n");
+                conta = parseInt(prompt("\tConta escolhida: "));
+                console.log("");
+
+                let valorASerDepositado = parseInt(prompt("\t• Insira o valor que deseja depositar: R$ "));
+
+                if (conta == 1) {
+                    console.log("\n\t>>> Conta_1 | SALDO ANTERIOR: R$ " + saldo1 + " | SALDO ATUAL: R$ " + depositarDinheiro(saldo1, valorASerDepositado) + "\n");
+                    console.log("-------------------------------------------------------------------------------------\n")
+                } else if (conta == 2) {
+                    console.log("\n\t>>> Conta_2 | SALDO ANTERIOR: R$ " + saldo2 + " | SALDO ATUAL: R$ " + depositarDinheiro(saldo2, valorASerDepositado) + "\n");
+                    console.log("-------------------------------------------------------------------------------------\n")
+                } else {
+                    console.log("\n\t*** Depósito cancelado. Conta inexistente! Escolha uma conta válida! ***\n");
+                    console.log("-------------------------------------------------------------------------------------\n")
+                }
+
+                break;
+            case 4: // transferir
+                console.log("\n\t-------------------------------------------------------------------------\n");
+
+                console.log("\t• Selecione uma conta cadastrada para prosseguir: [1] Conta_1 | [2] Conta_2\n");
+                conta = parseInt(prompt("\tConta escolhida: "));
+                console.log("");
+
+                let valorASerTransferido = parseInt(prompt("\t• Insira o valor que deseja transferir: R$ "));
+
+                if (conta == 1) {
+                    console.log("\n\t>>> Conta_1 | SALDO ANTERIOR: R$ " + saldo1 + "\n");
+                    transferirDinheiro(saldo1, saldo2, valorASerTransferido);
+                    console.log("\t>>> Conta_1 | SALDO ATUAL: R$ " + consultarSaldo(saldo1) + "\n");
+                    console.log("-------------------------------------------------------------------------------------\n");
+                } else if (conta == 2) {
+                    console.log("\n\t>>> Conta_2 | SALDO ANTERIOR: R$ " + saldo2 + "\n");
+                    transferirDinheiro(saldo2, saldo1, valorASerTransferido);
+                    console.log("\t>>> Conta_2 | SALDO ATUAL: R$ " + consultarSaldo(saldo2) + "\n");
+                    console.log("-------------------------------------------------------------------------------------\n");
+                } else {
+                    console.log("\n\t*** Transferência cancelada. Conta inexistente! Escolha uma conta válida! ***\n");
+                    console.log("-------------------------------------------------------------------------------------\n");
+                }
+
+                break;
+            case 5: // listar
+                listarAcoes();
+                break;
+            default:
+                console.log("\n\t*** Transação não encontrada! Tente novamente! ***\n");
+                console.log("-------------------------------------------------------------------------------------\n");
+        }
+    } else if (continuarExecucao == 0) {
+        console.log("\n------------------- APLICATIVO FINALIZADO COM SUCESSO! ------------------------\n");
         return -1;
     } else {
-        console.log("Opcao inválido tente novamente!")
+        console.log("\n\t*** Opção inválida! Tente novamente! ***\n");
+        console.log("-------------------------------------------------------------------------------------\n");
     }
-    switch (opcao) {
-        case 1:
-            var conta = parseInt(prompt("Digite qual conta deseja ver o saldo  1 para conta 1 e 2 para conta 2: "))
-            if (conta == 1) {
-                console.log("Conta_1 o saldo é: R$ " + consultaSaldo(saldo1))
-            }
-            if (conta == 2) {
-                console.log("Conta_2 o saldo é: R$ " + consultaSaldo(saldo2))
-            } else {
-                console.log("Conta inexistente tente novamente")
-            }
-            break;
 
-        case 2:
-            var valor = parseInt(prompt("Digite o valor para saque: "))
-            var conta = parseInt(prompt("Digite qual conta deseja fazero saque:\n  1 para saque na conta_1 e 2 para saque na conta_2: "))
-
-            if (conta == 1) {
-                saldo1 = saque(saldo1, valor)
-                console.log("Saque na Conta_1 o saldo é: R$ " + consultaSaldo(saldo1))
-            }
-            else if (conta == 2) {
-                saldo2 = saque(saldo2, valor)
-                console.log("Saque na Conta_2 o saldo é: R$ " + consultaSaldo(saldo2))
-            } else {
-                console.log("Conta inexistente para saque tente novamente")
-            }
-            break;
-        case 3:
-            var valor = parseInt(prompt("Digite o valor para Deposito: "))
-            var conta = parseInt(prompt("Digite qual conta deseja fazer deposito: \n1 para depositar na conta_1 e 2 para depositar na conta_2: "))
-            if (conta == 1) {
-                console.log("Saldo antes do depoisto " + saldo1 + " O novo saldo é: R$ " + deposito(saldo1, valor))
-            }
-            if (conta == 2) {
-                console.log("Saldo antes do depoisto " + saldo2 + " O novo saldo é: R$ " + deposito(saldo2, valor))
-            } else {
-                console.log("Conta inexistente para deposito tente novamente")
-            }
-            break;
-
-        case 4:
-            var valor = parseInt(prompt("Digite o valor para trasferir: "))
-            var conta = parseInt(prompt("Digite 1 para trasferir da conta_1 para a conta_2 ou digite 2 para a trasferir da conta_2 para a conta_1: "))
-            if (conta == 1) {
-                console.log("Saldo da conta_1 antes da trasferencia era R$ " + saldo1)
-                transferencia(saldo1, saldo2, valor)
-                console.log("O novo saldo é: R$ " + consultaSaldo(saldo1))
-            }
-            if (conta == 2) {
-                console.log("Saldo da conta_1 antes da trasferencia era R$ " + saldo2)
-                transferencia(saldo2, saldo1, valor)
-                console.log("O novo saldo é: R$ " + consultaSaldo(saldo2))
-            } else {
-                console.log("Conta inexistente para trasferencia tente novamente")
-            }
-            break;
-        case 5:
-            historicoAcoes()
-            break;
-
-        default:
-            console.log("Opção inválida")
-            break;
-    }
+    continuarExecucao = parseInt(prompt("\tDeseja continuar? [1] Menu | [0] Sair "));
 }
