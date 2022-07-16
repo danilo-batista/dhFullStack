90270dd5f7d5323a158dd36ed049a29608e8723d
Danilo Batista <developer@danilobatista.com>
2022-07-16 14:36:08 -0300

    dhFS M02-U01 | Cinehouse: first commit with variables.

---
+++ module02/cinehouse/cinema.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/** Cinehouse Project 
+ * 
+ * 
+ * 
+*/
+
+/* Criação de variáveis */
+let catalogo = {
+    codigo: 0,
+    titulo: "",
+    duracao: 0,
+    atores: [],
+    anoDeLancamento: 0,
+    emCartaz: true
+}
