e9cc9b77df9ee8e24cbecb8f4a28b7d38dd05ade
Danilo Batista <developer@danilobatista.com>
2022-07-16 08:27:35 -0300

    dhFS M01-U06 | Exercise 1 - Half numbers.

---
+++ unid06_funcoes/exercise01_half.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**
+ * Você deve criar uma função chamada metade que irá receber um número
+como parâmetro e deve retornar a metade desse número.
+ */
+
+function half(number) {
+    return number / 2;
+}
+
+let halfNumber = half(6);
+console.log("A metade é " + halfNumber + ".");
